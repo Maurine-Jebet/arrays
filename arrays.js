ebd8e3515cc611a7ce0b53ade180987dd5711253
Maurine-Jebet <maurinejebett@gmai.com>
2023-03-17 21:03:40 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,24 @@
+'use strict'
+let array= [3,7,34,90,12] 
+let lastElement = array.slice(-1);
+console.log(lastElement);
+
+let arr2 =[true,"green","where",12,56]
+let lastelement= arr2.slice(-1);
+console.log(lastelement);
+
+const myPets= ["Cow","python","snake","dog"];
+console.log(myPets.join(''));
+
+let arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+let newArr = arr3.sort();
+console.log(newArr)
+
+
+
+
+let word= "lufituable";
+let v =word.split("") ;
+v.sort();
+console.log(v);
+
